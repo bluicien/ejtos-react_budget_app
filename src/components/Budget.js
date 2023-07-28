538b1d5b16369ab43f899d935018ba3abdebbe14
Brendon Luicien <bluicien@yahoo.co.nz>
2023-07-28 09:03:38 -0400

    React final project save updates

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,30 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+
+    function budgetChange(event) {
+    
+    const {value} = event.target
+                dispatch({
+                    type: 'SET_BUDGET',
+                    payload: value,
+                });
+            } 
+    
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+        <div className='alert alert-secondary' style={{display: 'flex', flexDirection: 'row'}}>
+            <label style={{width: '70px'}}>Budget:£</label>
+            <input
+            required='required'
+            name='budget'
+            type='number'
+            step="10"
+            id='budget'
+            value={budget}
+            style={{ marginLeft: '2rem' , size: 10}}
+            onChange={(event) => budgetChange(event)}>
+            </input>
         </div>
     );
 };
